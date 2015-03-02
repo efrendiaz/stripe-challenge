0cd018b0cb7370f2c4c28270a67c4e318bfc2bc7
Efren Diaz <efren088@gmail.com>
2015-03-02 10:44:11 -0500

    Add a stripe_id to the customer model and initialize it  by creating the customer on stripe.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,17 @@
 class Customer < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
+
+  before_create :create_stripe_customer
+
+  private
+
+  def create_stripe_customer
+    stripe_customer = CustomerService.new(:email => self.email, :name => self.name).create
+    if stripe_customer
+      self.stripe_id = stripe_customer.id
+    else
+      self.errors.add(:stripe_id, "unable to create stripe customer")
+    end
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
-
+require 'stripe_mock'
 RSpec.describe Customer, :type => :model do
+
+  before { StripeMock.start }
+  after { StripeMock.stop }
+
+  it "should create a stripe customer on creation" do
+    customer = Customer.create(email: "test@test.com", name: "MyNameIs")
+    expect(customer.stripe_id).not_to be_nil
+  end
 end
