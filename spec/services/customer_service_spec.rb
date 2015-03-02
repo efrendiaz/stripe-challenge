d644edd93c0643a0a684fa73c640e2a14d14ce49
Efren Diaz <efren088@gmail.com>
2015-03-02 11:15:56 -0500

    Remove customer service instance, it's no longer required.

--- spec/services/customer_service_spec.rb
+++ spec/services/customer_service_spec.rb
@@ -3,7 +3,6 @@ require 'stripe_mock'
 describe CustomerService do
   before { StripeMock.start }
   after { StripeMock.stop }
-  let(:service){ described_class.new(:email => "test@test.com", :name => "MyName")}
 
   describe "creating Customers" do
     it "should create a customer" do
